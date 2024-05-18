8412bf5fca1b72e08b507baae11789d0f66de302
silas <silasgebhart12@gmail.com>
2024-05-18 17:41:44 -0500

    test

--- public/index.js
+++ public/index.js
@@ -1,51 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Event listener for the "Fetch Random Recipes" button
+  // Event listener for the "Fetch Random Recipe" button
   document
-    .getElementById("fetchRecipesBtn")
-    .addEventListener("click", fetchRecipes);
+    .getElementById("fetchRecipeBtn")
+    .addEventListener("click", fetchRecipe);
 
   document
     .getElementById("darkModeBtn")
     .addEventListener("click", toggleDarkMode);
 });
 
-function fetchRecipes() {
+function fetchRecipe() {
   fetch("http://localhost:3000/recipes")
     .then((response) => response.json())
     .then((data) => {
-      displayRecipes(data);
+      displayRecipe(data);
     })
     .catch((error) => {
-      console.error("Error fetching recipes:", error);
+      console.error("Error fetching recipe:", error);
     });
 }
 
-function displayRecipes(recipes) {
+function displayRecipe(recipes) {
   const recipesContainer = document.getElementById("recipesContainer");
   recipesContainer.innerHTML = "";
 
-  recipes.forEach((recipe) => {
-    const recipeElement = document.createElement("div");
-    recipeElement.classList.add("recipe");
-    recipeElement.innerHTML = `
-      <h2>${recipe.title}</h2>
-      <p>${recipe.summary}</p>
-      <img src="${recipe.image}" alt="${recipe.title}">
-      <p><strong>Instructions:</strong> ${recipe.instructions}</p>
-    `;
+  const randomIndex = Math.floor(Math.random() * recipes.length);
+  const recipe = recipes[randomIndex];
 
-    // Event listener for increasing size on hover
-    recipeElement.addEventListener("mouseover", function () {
-      this.style.transform = "scale(1.05)"; // Increase size by 10%
-    });
+  const recipeElement = document.createElement("div");
+  recipeElement.classList.add("recipe");
+  recipeElement.innerHTML = `
+    <h2>${recipe.title}</h2>
+    <p>${recipe.summary}</p>
+    <img src="${recipe.image}" alt="${recipe.title}">
+    <p><strong>Instructions:</strong> ${recipe.instructions}</p>
+  `;
 
-    // Event listener for reverting size on mouseout
-    recipeElement.addEventListener("mouseout", function () {
-      this.style.transform = "scale(1)"; // Revert to original size
-    });
-
-    recipesContainer.appendChild(recipeElement);
+  // Event listener for increasing size on hover
+  recipeElement.addEventListener("mouseover", function () {
+    this.style.transform = "scale(1.05)"; // Increase size by 10%
   });
+
+  // Event listener for reverting size on mouseout
+  recipeElement.addEventListener("mouseout", function () {
+    this.style.transform = "scale(1)"; // Revert to original size
+  });
+
+  recipesContainer.appendChild(recipeElement);
 }
 
 function toggleDarkMode() {
